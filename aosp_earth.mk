642f2b311f53120177d6e2934a497c06bb3d4821
inoo-droid <inogaer@gmail.com>
2026-08-15 16:39:26 +0700

    Update and rename yaap_earth.mk to aosp_earth.mk

--- aosp_earth.mk
+++ aosp_earth.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+$(call inherit-product, $(CUSTOM_PRODUCT_DIR)/config/common_full_phone.mk)
 
-PRODUCT_NAME := yaap_earth
+PRODUCT_NAME := aosp_earth
 PRODUCT_DEVICE := earth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
